78766e904c0a8cfbf2800e2ad6755fc8559098f9
Cleo <cleotran05@gmail.com>
2022-09-07 17:48:31 -0400

    start/finished py jungle escape
    ez

---
+++ CT-JungleEscape PY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        bool hit = Physics.Raycast(transform.position, transform.forward, 2, 1 << 8);
+
+        if (hit)
+        {
+            hit = true;
+            Debug.Log("All clear,go");
+        }
+        else
+        {
+            hit = false;
+            Debug.LogWarning("Be careful man");
+        }
+    }
+}
