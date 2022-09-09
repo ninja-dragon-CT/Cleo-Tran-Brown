fa2f191e0b1f54237b8c5ae346e48a2207dc7993
Cleo <cleotran05@gmail.com>
2022-09-09 18:38:07 -0400

    start / finish PY

---
+++ CT - Ninja Run PY/Assets/Scripts/Pickups.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public ParticleSystem Pickup;
+
+    void Start()
+    {
+        Pickup.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Door"));
+
+            Destroy(other.gameObject);
+
+            Pickup.Play();
+        }
+
+    }
+
+
+}
