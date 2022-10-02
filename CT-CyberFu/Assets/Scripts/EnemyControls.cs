d77f1ad9b75eba6346529086b2d9f278190f95c7
Cleo <cleotran05@gmail.com>
2022-10-02 15:25:40 -0400

    Started CyberFu
    step 6a

---
+++ CT-CyberFu/Assets/Scripts/EnemyControls.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    public float speed = 5;
+    public float attackingDistance = 1;
+    public Vector3 direction;
+
+    private Animator animatorEnemy;
+    private Rigidbody rigidbodyEnemy;
+    private Transform target;
+    private bool isFollowingTarget;
+    private bool isAttackingTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
