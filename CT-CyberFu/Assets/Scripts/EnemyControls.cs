8c453d3c887198ac8bb7698cf271ee09ed2bfaae
Cleo <cleotran05@gmail.com>
2022-10-05 19:42:45 -0400

    continued Cyberfu 1
    step 11 a

--- CT-CyberFu/Assets/Scripts/EnemyControls.cs
+++ CT-CyberFu/Assets/Scripts/EnemyControls.cs
@@ -11,18 +11,41 @@ public class EnemyControls : MonoBehaviour
     private Animator animatorEnemy;
     private Rigidbody rigidbodyEnemy;
     private Transform target;
-    private bool isFollowingTarget;
+    public bool isFollowingTarget;
     private bool isAttackingTarget;
+    private float chasingPlayer = 0.01f;
+    public float currentAttackingTime;
+    public float maxAttaackingTime = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFollowingTarget = true;
+        currentAttackingTime = maxAttaackingTime;
+        animatorEnemy = GetComponentInChildren<Animator>();
+        rigidbodyEnemy = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FollowTarget()
+    {
+        if(!isFollowingTarget)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, target.position) >= attackingDistance)
+        {
+            direction = target.position - transform.position;
+            direction.y = 0;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 20);
+        }
     }
 }
