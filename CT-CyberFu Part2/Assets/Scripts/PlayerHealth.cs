b00855ebf48170411d8b26564cade273c878f6e4
Cleo <cleotran05@gmail.com>
2022-11-02 17:53:13 -0400

    finished cyber 2

---
+++ CT-CyberFu Part2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentPlayerHealth = 10;
+    public int enemyDamge = 1;
+
+    public PlayerExplosionParticles particles;
+
+    private Animator playerAnimator;
+   
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
+    }
+    
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamge;
+        playerAnimator.SetTrigger("Hit");
+
+        if (currentPlayerHealth <= 0)
+        {
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HitCollider")
+        {
+            HurtPlayer();
+        }
+    }
+}
